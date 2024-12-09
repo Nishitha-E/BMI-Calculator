5a0d7e0b78009bf462e6d3f4dc1142f4bf008cb7
Nishitha-E <nishithaeerishetty@gmail.com>
2024-12-09 17:24:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById("bmiform").addEventListener('submit', function (e) {
+    e.preventDefault();
+    const gender = document.getElementById('gender').value;
+    const age = parseInt(document.getElementById('age').value);
+    const heightFeet = parseInt(document.getElementById('height-feet').value);
+    const heightInches = parseInt(document.getElementById('height-inches').value);
+    const weight = parseFloat(document.getElementById('weight').value);
+
+    if (!gender || isNaN(age) || isNaN(heightFeet) || isNaN(heightInches) || isNaN(weight)) {
+        alert("Please fill out all fields with valid values.");
+        return;
+    }
+
+    const heightInMeters = ((heightFeet * 12) + heightInches) * 0.0254;
+    const bmi = weight / (heightInMeters * heightInMeters);
+    const resultElement = document.getElementById("result");
+
+    let category = '';
+    if (bmi < 18.5) {
+        category = 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        category = 'Normal Weight';
+    } else if (bmi >= 25 && bmi < 29.9) {
+        category = 'Overweight';
+    } else {
+        category = 'Obese';
+    }
+
+    const resultMessage = `Your BMI is: ${bmi.toFixed(2)}<br>Category: ${category}`;
+    resultElement.innerHTML = resultMessage;
+});
